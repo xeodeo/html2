a9d32465fb1b9e5b0badc87301bb6f959f694d7a
xeodeo <pintolopez89@gmail.com>
2025-08-30 10:27:36 -0500

    Mejora de manejo de carpetas

---
+++ configs/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost'; 
+$dbname = 'tienda'; 
+$username = 'root'; 
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("SET NAMES 'utf8'");
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+
+?>
